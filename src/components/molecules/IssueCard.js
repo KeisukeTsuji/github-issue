82a5ac77549ef220d0bb16f401cf58e7a373a709
KeisukeTsuji <kei.iwmmd@gmail.com>
2020-11-08 15:21:28 +0900

    IssueCard.jsが冗長だったのでclass -> function componentに変更

--- src/components/molecules/IssueCard.js
+++ src/components/molecules/IssueCard.js
@@ -1,25 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-class IssueCard extends Component {
-  render() {
-    return (
-      <Link to={"issues/" + this.props.number}>
-        <article>
-          <h4>{this.props.title}</h4>
-          <p>{this.props.number}</p>
-        </article>
-        <style jsx>{`
-          article {
-            max-width: 960px;
-            width: 100%;
-            padding: 16px;
-            border: 1px solid gray;
-          }
-        `}</style>
-      </Link>
-    );
-  }
-}
+const IssueCard = (props) => {
+  return (
+    <Link to={`issues/${props.number}`}>
+      <article>
+        <h4>{props.title}</h4>
+        <p>{props.number}</p>
+      </article>
+      <style>{`
+        article {
+          max-width: 960px;
+          width: 100%;
+          padding: 16px;
+          border: 1px solid gray;
+        }
+      `}</style>
+    </Link>
+  );
+};
 
 export default IssueCard;
