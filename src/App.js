63ad5fd5ed21063cc02a4d3bf9597c7c49a9e708
KeisukeTsuji <kei.iwmmd@gmail.com>
2020-11-10 10:44:12 +0900

    github pages関係の設定

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import ListPage from "./pages/ListPage";
 import IssueDetail from "./pages/IssueDetail";
 import Loading from "./components/Loading";
 
+const ROUTER_BASENAME =
+  process.env.NODE_ENV === 'development' ? '/' : '/github-issues';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
+        <Router basename={ROUTER_BASENAME}>
           <RecoilRoot>
             <Switch>
               <Route exact path="/issues/:number" component={IssueDetail} />
               <Route exact path="/issues" component={ListPage} />
+              <Redirect to="/issues?page=1" />
             </Switch>
             <Loading />
           </RecoilRoot>
